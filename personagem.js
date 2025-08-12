e0bfd6ee51eb03a5fe108e4113ac1978fbad99f5
ArthurFritz8 <geminepestudante5@gmail.com>
2025-08-11 23:22:47 -0300

    Update personagem.js

--- personagem.js
+++ personagem.js
@@ -1,9 +1,11 @@
-// personagem.js
-
+// personagem.js - Versão 2
 const personagem = {
   nome: "AstroDev",
   planeta: "X-15",
-  nivel: 5
+  nivel: 8, // ALTERADO DE 5 PARA 8
+  poder: "Super Velocidade" // ADICIONADO
 };
 
 console.log(`Nosso herói é ${personagem.nome} do planeta ${personagem.planeta}.`);
+// ADICIONADO
+console.log(`Seu poder agora é ${personagem.poder} e seu nível subiu para ${personagem.nivel}!`);
