01b0bb025519351c51f08b17d74709064f750a82
ArthurLFritz <100100brr@gmail.com>
2025-08-11 23:09:56 -0300

    Adicionando meu personagem

---
+++ personagem.js
@@ -0,0 +1,9 @@
+// personagem.js
+
+const personagem = {
+  nome: "AstroDev",
+  planeta: "X-15",
+  nivel: 5
+};
+
+console.log(`Nosso herói é ${personagem.nome} do planeta ${personagem.planeta}.`);
